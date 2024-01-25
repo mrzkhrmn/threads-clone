906ee0e968d88e2e433fc3535fc5762534ccdbbe
mrzkhrmn <mrzkhrmn@hotmail..com>
2024-01-25 14:43:16 +0300

    creating protected route and follow/unfollow api route

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -65,3 +65,45 @@ export const loginUser = async (req, res) => {
     console.log("Error in loginUser: " + error.message);
   }
 };
+
+export const logoutUser = async (req, res) => {
+  try {
+    res.cookie("jwt", "", { maxAge: 1 });
+    res.status(200).json({ message: "Logged out successfully!" });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+    console.log("Error in logoutUser: " + error.message);
+  }
+};
+
+export const followUnfollowUser = async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    const userToFollow = await User.findById(id);
+    const currentUser = await User.findById(req.user._id);
+
+    if (id === req.user._id)
+      return res
+        .status(400)
+        .json({ message: "You cannot follow/unfollow yourself" });
+
+    if (!userToFollow || !currentUser)
+      return res.status(400).json({ message: "User not found!" });
+
+    const isFollowing = currentUser.following.includes(id);
+
+    if (isFollowing) {
+      await User.findByIdAndUpdate(id, { $pull: { followers: req.user._id } });
+      await User.findByIdAndUpdate(req.user._id, { $pull: { following: id } });
+      res.status(200).json({ message: "User unfollowed successfully!" });
+    } else {
+      await User.findByIdAndUpdate(id, { $push: { followers: req.user._id } });
+      await User.findByIdAndUpdate(req.user._id, { $push: { following: id } });
+      res.status(200).json({ message: "User followed successfully!" });
+    }
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+    console.log("Error in followUnfollowUser: " + error.message);
+  }
+};
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,9 +1,17 @@
 import express from "express";
-import { loginUser, signupUser } from "../controllers/userController.js";
+import {
+  followUnfollowUser,
+  loginUser,
+  logoutUser,
+  signupUser,
+} from "../controllers/userController.js";
+import { protectedRoute } from "../middlewares/protectedRoute.js";
 
 const router = express.Router();
 
 router.post("/signup", signupUser);
 router.post("/login", loginUser);
+router.post("/logout", logoutUser);
+router.post("/follow/:id", protectedRoute, followUnfollowUser);
 
 export default router;
